0e61a9bc6854bc32a592403a1c32cc63777b6da7
Juan Ramos <juan_cr_1997@hotmail.com>
2025-06-20 21:48:04 -0500

    Prueba de bulk Transacciones

--- src/routes/transaction.router.js
+++ src/routes/transaction.router.js
@@ -41,8 +41,7 @@ routerTransaction.route('/')
     .get(getVentasTransactions);
 
     routerTransaction.route('/bulk')
-    
-        .post(createManyContracts);
+    .post(createManyContracts);
     
 routerTransaction.route('/empeno')
     .get(getEmpeñoTransactions);
